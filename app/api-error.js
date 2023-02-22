6c3e32e5469bc460d5b568dd2b9e564d963b7081
Hoang Kha <khavo12134@gmail.com>
2023-02-22 17:10:13 +0700

    Cai dat cac handler truy xuat CSDL

--- app/api-error.js
+++ app/api-error.js
@@ -1,5 +1,5 @@
-class ApiError extends Error{
-    constructor(statusCode, message){
+class ApiError extends Error {
+    constructor(statusCode, message) {
         super();
         this.statusCode = statusCode;
         this.message = message;
