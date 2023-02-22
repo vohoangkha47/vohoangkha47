e16a36ff04b75ef322567126f9e738cd3512b422
Hoang Kha <khavo12134@gmail.com>
2023-02-22 11:53:04 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +11,6 @@ app.use(express.json());
 app.get("/",(req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
